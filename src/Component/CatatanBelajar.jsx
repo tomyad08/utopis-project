ba1419088108594dce34cfa7198fa7660564a478
LAPTOP-JF5K0512\tomyt <tomyad08@gmail.com>
2024-04-03 22:10:17 +0700

    ubah

--- src/Component/CatatanBelajar.jsx
+++ src/Component/CatatanBelajar.jsx
@@ -1,45 +1,49 @@
+import { useLocation } from "react-router-dom";
+
 const CatatanBelajar = () => {
+  const location = useLocation();
+
   const data = [
     {
       id: 1,
       nama: "PU",
-      link: "",
+      link: location.state.pu,
       color: "red",
     },
     {
       id: 2,
       nama: "PM",
-      link: "",
+      link: location.state.pm,
       color: "blue",
     },
     {
       id: 3,
       nama: "PK",
-      link: "",
+      link: location.state.pk,
       color: "yellow",
     },
     {
       id: 4,
       nama: "PBM",
-      link: "",
+      link: location.state.pbm,
       color: "grey",
     },
     {
       id: 5,
       nama: "PPU",
-      link: "",
+      link: location.state.ppu,
       color: "orange",
     },
     {
       id: 6,
       nama: "LBING",
-      link: "",
+      link: location.state.lbing,
       color: "black",
     },
     {
       id: 7,
       nama: "LBIND",
-      link: "",
+      link: location.state.lbind,
       color: "green",
     },
   ];
@@ -48,22 +52,24 @@ const CatatanBelajar = () => {
       <h1 className="text-sm font-semibold text-blue-700">Catatan Belajar</h1>
       <div className="columns-4 md:columns-7 justify-center gap-1 p-2 bg-blue-100 rounded-lg">
         {data.map((value) => (
-          <div className="flex justify-center">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="70"
-                height="70"
-                fill={value.color}
-                class="bi bi-folder-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
-              </svg>
-              <p className="text-center" style={{ fontSize: "12px" }}>
-                {value.nama}
-              </p>
-            </div>
+          <div className="flex justify-center" key={value.id}>
+            <a href={value.link}>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="70"
+                  height="70"
+                  fill={value.color}
+                  class="bi bi-folder-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
+                </svg>
+                <p className="text-center" style={{ fontSize: "12px" }}>
+                  {value.nama}
+                </p>
+              </div>
+            </a>
           </div>
         ))}
       </div>
