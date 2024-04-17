5c49094a5c1830e3e12e2dad5a45719b458df422
LAPTOP-JF5K0512\tomyt <tomyad08@gmail.com>
2024-04-17 09:45:47 +0700

    ubah

--- src/Component/Report.jsx
+++ src/Component/Report.jsx
@@ -5,14 +5,22 @@ import { useLocation } from "react-router-dom";
 const Report = () => {
   const location = useLocation();
   const [datas, setDatas] = useState("");
-  let y = [];
+  const [filter, setFilter] = useState("2");
+  const [rate, setRate] = useState(0);
+
   const FilterFunction = (data) => {
+    setDatas("");
+    let y = 0;
     const x = data.filter((value) => {
-      if (value.nama_lengkap === location.state.nama_lengkap) {
+      if (
+        value.nama_lengkap === location.state.nama_lengkap &&
+        value.tryout === Number(filter)
+      ) {
+        y += value.nilai;
         return value;
       }
     });
-
+    setRate(y / 7);
     setDatas(x);
   };
   useEffect(() => {
@@ -23,24 +31,28 @@ const Report = () => {
       .then((data) => {
         FilterFunction(data);
       });
-  }, []);
+  }, [filter]);
+
   return (
     <div className="p-5">
-      <h1 className="text-sm font-semibold text-blue-700">Rapor TryOut ke-2</h1>
+      <h1 className="text-sm font-semibold text-blue-700">{`Rapor TryOut ke-${filter}`}</h1>
       <div className="p-4 bg-blue-100 rounded-lg">
         <div className="flex justify-end">
-          <select className="p-2 text-sm focus:outline-none text-end">
+          <select
+            className="p-2 text-sm focus:outline-none text-end rounded-xl"
+            onChange={(e) => setFilter(e.target.value)}
+          >
             <option>Silahkan Pilih TO</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+            <option value="1">TryOut-1</option>
+            <option value="2">TryOut-2</option>
+            <option value="3">TryOut-3</option>
+            <option value="4">TryOut-4</option>
+            <option value="5">TryOut-5</option>
           </select>
         </div>
         <div className="flex justify-center">
           {datas ? (
-            <table>
+            <table style={{ fontSize: "12px" }}>
               <thead>
                 <tr>
                   <th className="p-2">Mata Pelajaran</th>
@@ -69,6 +81,14 @@ const Report = () => {
           ) : (
             <p>Loading...</p>
           )}
+        </div>
+        <div>
+          <h1 className="text-center font-bold text-3xl mt-5">
+            Nilai Rata-rata:
+          </h1>
+          <h1 className="text-center font-bold text-8xl text-blue-800">
+            {Math.floor(rate)}
+          </h1>
         </div>
       </div>
     </div>
