dd68da02c9384fe73be7b67fdbd972dcea728e3d
LAPTOP-JF5K0512\tomyt <tomyad08@gmail.com>
2024-06-06 22:47:09 +0700

    ubah

--- src/Component/quiz.jsx
+++ src/Component/quiz.jsx
@@ -1,9 +1,12 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { Mapel } from "../DataStatics/Menu";
+import { useEffect, useState } from "react";
+import { ENPOINTS } from "../DataStatics/endpoints";
 
 const Quiz = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [datas, setDatas] = useState("");
   const handleSelect = (value) => {
     const data = {
       datasiswa: location.state,
@@ -15,26 +18,45 @@ const Quiz = () => {
       state: data,
     });
   };
+
+  useEffect(() => {
+    fetch(ENPOINTS.LATSOL, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setDatas(data);
+      });
+  }, []);
+
   return (
     <div className="px-5 pb-2">
       <h1 className="text-sm font-semibold text-blue-700">Quiz</h1>
       <div className="columns-7 justify-center w-full bg-blue-100 p-4 rounded-lg">
-        {Mapel.map((value) => (
-          <div key={value.id} onClick={() => handleSelect(value)}>
-            <div className="flex justify-center text-center text-sm mb-3">
-              <img
-                src={value.picture}
-                className="w-30 rounded-xl border-2 border-blue-300"
-              />
-            </div>
-            <p
-              className=" text-center text-blue-900 font-semibold"
-              style={{ fontSize: "10px" }}
-            >
-              {value.inisial}
-            </p>
-          </div>
-        ))}
+        {datas ? (
+          <>
+            {datas.map((value) => (
+              <div key={value.id} onClick={() => handleSelect(value)}>
+                <div className="flex justify-center text-center text-sm mb-3">
+                  <img
+                    src={value.picture}
+                    className="w-30 rounded-xl border-2 border-blue-300"
+                  />
+                </div>
+                <p
+                  className=" text-center text-blue-900 font-semibold"
+                  style={{ fontSize: "10px" }}
+                >
+                  {value.mapel}
+                </p>
+              </div>
+            ))}
+          </>
+        ) : (
+          <p className="text-sm font-semibold text-blue-500 text-center">
+            Loading
+          </p>
+        )}
       </div>
     </div>
   );
--- src/DataStatics/endpoints.jsx
+++ src/DataStatics/endpoints.jsx
@@ -1,17 +1,10 @@
 exports.ENPOINTS = {
   MERGETO:
     "https://script.google.com/macros/s/AKfycbyNFVOWWi2WuRCROD1P0KX2WIS0xo3XEyIFdDCIx8oJLoi0638fyWhg--ORICy_j0Rp/exec",
-  PU: "https://script.google.com/macros/s/AKfycbyeYCmzsyIcD9l5vureKTAELbrc6pF4LLyZPcbThzSXqjXdpjIDFKngb6JVK0Go83eL/exec",
-  PM: "https://script.google.com/macros/s/AKfycbwOKUuRrMAXH0S-qG-MRtmsLG9QmTLr-GD6eqMXz6a6vD1Uhzb9MZzQeUyElkAQ7bFq2w/exec",
-  PK: "https://script.google.com/macros/s/AKfycbyjLAqGOkntCU-9VSsv-LOo4iPjyF3YwR13nCo8i3OKan5AIMLuJQm1RaaSq5ZL9AzEvg/exec",
-  PPU: "https://script.google.com/macros/s/AKfycbxeBkh5tDea2WSi2F3LfM4o408NgBV585seOhnY3mKO6CqR4Sj11EtLDrZCClKxHuq9kA/exec",
-  PBM: "https://script.google.com/macros/s/AKfycby5f00USFR2q2T9kh4wmmdyOn1pFzAnhgTYTrPzNhNLDZ5iK018KdvcOyF0VJOY3RKrMg/exec",
-  LBIND:
-    "https://script.google.com/macros/s/AKfycbzD4Cbi_gIHhE_tdnXOz38xYAym_iVignRClsRXCcK1xRoHkoaBEkQHEzWhLRpv81-K6w/exec",
-  LBING:
-    "https://script.google.com/macros/s/AKfycbxUHfmwsoVi6Upi4mCHtpINscHoJKN6zIiP5QdWZ3wT0slKgvV3Aa0RvOm6QLxXt5sG/exec",
   LOGIN:
-    "https://script.google.com/macros/s/AKfycby5jWhuXD-ECtZ6W30ZrRInX4D_iNmYse0uTd9R1PZiEQVnKJcc49W_0QUrKViyF7LW3Q/exec",
+    "https://script.google.com/macros/s/AKfycbxbhkhJHf68izHLzg5pJOWP3h3A6ZVrmen6idsRXxYORcfqHL_uWXr6TOdk0Da0KS322Q/exec",
   REPORT:
     "https://script.google.com/macros/s/AKfycbxZizt3yszQxlGY5NjqIQJlBeBSA9R2KQp6BeFDNZPBjdOis8ZO3KI2VyKandddi2rt/exec",
+  LATSOL:
+    "https://script.google.com/macros/s/AKfycbxoeE0nOSpiHYsZe7CPZTlc6r0xFhI1VdSYhqy5via9rMsFpTha8Wv7YswbLGd72jW2lQ/exec",
 };
