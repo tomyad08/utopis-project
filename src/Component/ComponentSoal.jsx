fe7f3f65ed5f093ecca88778ae226fc96358c3ff
LAPTOP-JF5K0512\tomyt <tomyad08@gmail.com>
2024-09-01 14:22:54 +0700

    ubah

--- src/Component/ComponentSoal.jsx
+++ src/Component/ComponentSoal.jsx
@@ -3,11 +3,7 @@ const ComponentSoal = ({ input }) => {
 
   return (
     <div>
-      <div
-        className={` p-5 xl:p-2 mb-1 hover:scale-105 text-center hover:drop-shadow-xl font-semibold rounded-lg hover:bg-blue-600 cursor-pointer ${
-          Data.select ? "bg-blue-600 text-white" : ""
-        }`}
-      >
+      <div className="p-5 xl:p-2 mb-1 hover:scale-105 text-center hover:drop-shadow-xl font-semibold rounded-lg hover:bg-blue-600 cursor-pointer">
         {Data.no_soal}
       </div>
     </div>
